8afa3db39f063a19af652964356c5bb690523b70
joelochieng361 <joelochieng361@gmail.com>
2026-02-04 13:23:41 +0300

    first commit

---
+++ lesson1.py
@@ -0,0 +1,5 @@
+# what is a variable. They are variables which act as containers that hold values. a variable is associated with some values
+#  We use the assignment (=) opperator to assign values
+message = "hello there! hope everyone is fine"
+# Anything that is inside quotes in python is called a string
+print(message)
---
+++ python.py
@@ -0,0 +1,4 @@
+print("hello world")
+
+# this is a comment
+"""this is also a comment"""
